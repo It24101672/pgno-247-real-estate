c511d1fa8196e77da00c857a1c9c95b38b5d7a59
IT24103580 <it24103580@my.sliit.lk>
2025-05-18 21:35:58 +0530

    Add CustomUserDetailsService.java

---
+++ src/main/java/com/realEstate/util/CustomUserDetailsService.java
@@ -0,0 +1,31 @@
+package com.realEstate.util;
+
+import com.realEstate.model.User;
+import com.realEstate.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Primary;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.*;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Primary
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + email));
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getEmail(),
+                user.getPassword(),
+                List.of(new SimpleGrantedAuthority(user.getRole()))
+        );
+    }
+}
